29d8d6850a5eb684524dcba11299af029d04a9b2
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-10-13 01:11:59 +1100

    sidebar nav with categories and subcategories, header update with mdern design account ui

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoConnect from "./src/config/mongoConfig.js";
 import config from "./src/config/config.js";
 import authRouter from "./src/routes/authRouter.js";
 import customerRouter from "./src/routes/customerRouter.js";
+import categoryRouter from "./src/routes/categoryRouter.js";
 
 const app = express();
 app.use(cors());
@@ -19,6 +20,9 @@ app.use("/api/v1/auth", authRouter);
 
 //user routes
 app.use("/api/v1/customer", customerRouter);
+
+//category router
+app.use("/api/v1/category", categoryRouter);
 
 mongoConnect()
   .then(() => {
