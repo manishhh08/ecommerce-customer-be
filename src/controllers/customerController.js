eaebfef90ed9943f51c581ddd91f6f5cb8c472d1
Subin Bajracharya <linussbajracharya@gmail.com>
2025-10-21 10:03:17 +1100

    customer profile implemented

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,4 +1,4 @@
-// import { getAllUsers } from "../models/customers/customerModel";
+import { updateById } from "../models/customers/customerModel.js";
 
 export const getCustomerDetail = (req, res) => {
   try {
@@ -19,6 +19,34 @@ export const getCustomerDetail = (req, res) => {
     return res.status(500).json({
       status: "error",
       message: "Failed to retrieve user details",
+    });
+  }
+};
+
+export const updateCustomerDetail = async (req, res) => {
+  try {
+    const userId = req.user?._id;
+    const update = req.body;
+    console.log(update);
+
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found!",
+      });
+    }
+    const updatedUser = await updateById(userId, update, { new: true });
+    console.log(updatedUser);
+
+    return res.json({
+      status: "success",
+      message: "User details updated successfully",
+      customer: updatedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Failed to update user details",
     });
   }
 };
