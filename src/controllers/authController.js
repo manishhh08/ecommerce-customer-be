660b7076fd4caf573a4c9eb0d6139bee3fd08b23
Prashikaa <prasika.adhikari11@gmail.com>
2025-11-04 11:45:12 +1100

    Password feature

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,6 +10,7 @@ import { encodeFunction, decodeFunction } from "../utils/encodeHelper.js";
 import { createAccessToken, createRefreshToken } from "../utils/jwt.js";
 import { v4 as uuidv4 } from "uuid";
 import crypto from "crypto";
+import bcrypt from "bcryptjs";
 
 export const createNewCustomer = async (req, res) => {
   try {
@@ -68,9 +69,15 @@ export const verifyCustomer = async (req, res) => {
     const { token, email } = req.body;
     const user = await findByFilter({ email: email });
     if (!user) {
-      res.status(500).json({ status: "error", message: "User not found" });
+      return res
+        .status(404)
+        .json({ status: "error", message: "User not found" });
     }
+
     if (user.verificationToken === token) {
+      console.log("Verification token sent by user:", token);
+      console.log("Verification token in DB:", user.verificationToken);
+
       const updateIsVerified = await updateById(user._id, { isVerified: true });
       if (updateIsVerified)
         return res
@@ -98,7 +105,7 @@ export const loginCustomer = async (req, res) => {
         .json({ status: "error", message: "Invalid credentials" });
     }
 
-    const isPasswordValid = decodeFunction(password, user.password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
       return res
@@ -229,4 +236,13 @@ export const resetPassword = async (req, res) => {
       message: "Failed to reset password",
     });
   }
+};
+export const password = async (req, res) => {
+  const token = req.query.token;
+  res.send(`<form action="/api/auth/reset-password" method="POST">
+              <input type="hidden" name="token" value="${token}" />
+              <input type="email" name="email" />
+              <input type="password" name="newPassword" />
+              <button type="submit">Reset</button>
+            </form>`);
 };
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,6 +3,7 @@ import {
   createNewCustomer,
   forgotPassword,
   loginCustomer,
+  password,
   resetPassword,
   verifyCustomer,
 } from "../controllers/authController.js";
@@ -19,14 +20,7 @@ router.post("/verify", verifyUserValidation, verifyCustomer);
 router.post("/login", loginValidation, loginCustomer);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
-router.get("/reset-password", (req, res) => {
-  const token = req.query.token;
-  res.send(`<form action="/api/auth/reset-password" method="POST">
-              <input type="hidden" name="token" value="${token}" />
-              <input type="email" name="email" />
-              <input type="password" name="newPassword" />
-              <button type="submit">Reset</button>
-            </form>`);
-});
+
+router.get("/password", password);
 
 export default router;
